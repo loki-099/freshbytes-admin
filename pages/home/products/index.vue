a7dab96dcc0e9b1ba21796ccd4812f99a3843900
melizagabinete <147030873+melizagabinete@users.noreply.github.com>
2025-06-18 22:10:24 +0800

    Update index.vue

--- pages/home/products/index.vue
+++ pages/home/products/index.vue
@@ -2,6 +2,17 @@
     definePageMeta({
     layout: "home",
 })
+    const products = ref([
+  {
+    id: 1,
+    name: "Papaya",
+    category: "Fruits",
+    price: "9",
+    stock: "123",
+    totalSales: "$199.99",
+    status: "Inactive"
+  },
+])
 </script>
 
 <template>
@@ -71,5 +82,146 @@
                     </div>
                 </div>
                 </div>
+                <div class="flex flex-wrap items-center justify-between gap-4 mb-4">
+                    <div class="flex gap-2 flex-wrap">
+                        <input type="text" placeholder="Search" class="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                        <select class="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <option>Category</option>
+                        <option>Fruits</option>
+                        <option>Vegetables</option>
+                        </select>
+                        <select class="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <option>Sub - Category</option>
+                        <option>Root Vegetables</option>
+                        <option>Leafy Greens</option>
+                        <option>Cruciferous Vegetables</option>
+                        <option>Nightshades</option>
+                        </select>
+                        <select class="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <option>Price</option>
+                        <option>Low to High</option>
+                        <option>High to Low</option>
+                        </select>
+                        <select class="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+                        <option>Status</option>
+                        <option>Inactive</option>
+                        <option>Active</option>
+                        <option>Sale</option>
+                        <option>Discounted</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="flex flex-col">
+                    <div class="overflow-x-auto">
+                        <div class="inline-block min-w-full align-middle">
+                            <div class="overflow-hidden shadow">
+                                <table class="min-w-full divide-y divide-gray-200 table-fixed dark:divide-gray-600">
+                                    <thead class="bg-gray-100 dark:bg-gray-700">
+                                        <tr>
+                                            <th scope="col" class="p-4">
+                                                <div class="flex items-center">
+                                                    <input id="checkbox-all" aria-describedby="checkbox-1" type="checkbox" class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600">
+                                                    <label for="checkbox-all" class="sr-only">checkbox</label>
+                                                </div>
+                                            </th>
+                                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                                                Product Name
+                                            </th>
+                                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                                                Product ID
+                                            </th>
 
+                                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                                                Category
+                                            </th>
+                                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                                                Price
+                                            </th>
+                                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                                                Stock
+                                            </th>
+                                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                                                Total Sales
+                                            </th>
+                                            <th scope="col" class="p-4 text-xs font-medium text-left text-gray-500 uppercase dark:text-gray-400">
+                                                Status
+                                            </th>
+                                            <th scope="col" class="p-4 text-xs font-medium text-center text-gray-500 uppercase dark:text-gray-400">
+                                                Actions
+                                            </th>
+                                        </tr>
+                                    </thead>
+                                    <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+                                        <tr v-for="product in products" :key="product.id" class="hover:bg-gray-100 dark:hover:bg-gray-700">
+                                            <td class="w-4 p-4">
+                                            <div class="flex items-center">
+                                                <input
+                                                :id="`checkbox-${product.id}`"
+                                                type="checkbox"
+                                                class="w-4 h-4 border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:focus:ring-primary-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600"
+                                                />
+                                                <label :for="`checkbox-${product.id}`" class="sr-only">checkbox</label>
+                                            </div>
+                                            </td>
+                                            <td class="p-4 text-sm font-semibold text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ product.name }}
+                                            </td>
+                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ product.id }}
+                                            </td>
+                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ product.category }}
+                                            </td>
+                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ product.price }}
+                                            </td>
+                                            <td class="p-4 text-base font-normal text-gray-900 whitespace-nowrap dark:text-gray-400">
+                                            {{ product.stock }}
+                                            </td>
+                                            <td class="p-4 text-base font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            {{ product.totalSales }}
+                                            </td>
+                                            <td class="p-4 text-base font-medium whitespace-nowrap">
+                                            <span
+                                                :class="product.status === 'Inactive'
+                                                ? 'text-red-600 font-semibold'
+                                                : 'text-green-600 font-semibold'"
+                                            >
+                                                {{ product.status }}
+                                            </span>
+                                            </td>
+                                            <td class="p-4 space-x-2 whitespace-nowrap">
+                                            <button
+                                                type="button"
+                                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-[#29000] rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                                            >
+                                                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                                                <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+                                                <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
+                                                </svg>
+                                                Update
+                                            </button>
+
+                                            <button
+                                                type="button"
+                                                class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:ring-red-300 dark:focus:ring-red-900"
+                                            >
+                                                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                                                <path
+                                                    fill-rule="evenodd"
+                                                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                                                    clip-rule="evenodd"
+                                                ></path>
+                                                </svg>
+                                                Delete item
+                                            </button>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 </template>
